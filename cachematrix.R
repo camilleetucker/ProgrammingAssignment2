b5949f36b538380eaa14143aa5303a35981b506c
camilleetucker <camilleetucker@gmail.com>
2015-07-25 15:19:06 -0400

    Cache Matrix Tusk
    Submission for Programming Assignment #2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix function sets the value of a matrix, 
+## calculates its inverse, and then caches that value.
 
-## Write a short comment describing this function
+## The cacheSolve function searches the cache for the inverse 
+## of 'x'. If the solution is in the cache, it is returned. 
+## If the inverse is not in the cache, the solution is calculated
+## and returned.
+
+## This function calculates the inverse of a matrix and caches it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## This function returns the cached solution. If it does not exist,
+## the solution is calculated and returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
